141518613593c8c81efbacd4a0f8ccfb663152bf
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-05 08:37:01 +0200

    finishing styling media query & btn, starting setting up the formastic API

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+//  Get quote from API
+async function getQuote() {
+
+  const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=eng&format=json';
+};
+
+try {
+  const response = await fetch(apiURL);
+  const data = await response.json();
+  console.log(data);
+
+} catch (error){
+  console.log('Whoops, nno quote!!!', error);
+}
+
+
+
+//  On load
+getQuote();
