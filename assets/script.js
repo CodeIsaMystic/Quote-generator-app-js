03fe2d701c6c55fe32e86b982e89a74c80b31b1c
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-07 07:18:30 +0200

    puttin our own proxy server on heroku

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
 //  Get quote from API
 async function getQuote() {
 
+  const proxyURL = 'https://pacific-thicket-16003.herokuapp.com/';
   const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=eng&format=json';
-};
-
-try {
-  const response = await fetch(apiURL);
-  const data = await response.json();
-  console.log(data);
-
-} catch (error){
-  console.log('Whoops, nno quote!!!', error);
+  
+  try {
+    const response = await fetch(proxyURL + apiURL);
+    const data = await response.json();
+    console.log(data);
+    
+  } catch (error){
+    getQuote();
+    console.log('Whoops, no quote!!!', error);
+  }
 }
 
 
